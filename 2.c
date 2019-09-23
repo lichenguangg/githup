8ec408741fd95cd550cdbda7b18304b26c1d7f41
lichenguangg <752120697@qq.com>
2019-09-23 04:19:15 -0400

    modified 2.c

---
+++ 2.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	#	 FileName	: 2.c
+	#	 Author		: lichenguang 
+	#	 Email		: 18883765905@163.com 
+	#	 Created	: Mon 23 Sep 2019 04:09:33 AM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello....\n");
+    return 0;
+}
