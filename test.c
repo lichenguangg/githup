70a508b3cf7fb540bd54fbe01cbf076a50018bae
lichenguangg <752120697@qq.com>
2019-09-23 03:35:35 -0400

    modified test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: lichenguang 
+	#	 Email		: 18883765905@163.com 
+	#	 Created	: Mon 23 Sep 2019 03:32:32 AM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello world \n");
+    return 0;
+}
